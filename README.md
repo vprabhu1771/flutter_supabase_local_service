306677600b30e19695d75dec154a2b9b20fe7782
Prabhu <vprabhu1771@gmail.com>
2025-02-24 17:17:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_supabase_local_service
+ 
---
+++ android/app/src/main/kotlin/com/example/flutter_supabase_local_service/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_supabase_local_service
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
