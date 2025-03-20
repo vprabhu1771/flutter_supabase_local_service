5277cd90696b548843981c834b569c6a9d4113a7
Prabhu <vprabhu1771@gmail.com>
2025-03-20 15:25:05 +0530

    Update CustomLocationAppBar.dart

--- lib/widgets/CustomLocationAppBar.dart
+++ lib/widgets/CustomLocationAppBar.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
 
+import 'CurrentAddressMapScreen.dart';
+
 class CustomLocationAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String address;
 
-  const CustomLocationAppBar({super.key, required this.address});
+  final double latitude;
+  final double longitude;
+
+  const CustomLocationAppBar({
+    super.key,
+    required this.address,
+    required this.latitude,
+    required this.longitude
+  });
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text(
-        address,
-        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-        overflow: TextOverflow.ellipsis, // Prevents text overflow
+      title: GestureDetector(
+        child:  Text(
+          address,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+          overflow: TextOverflow.ellipsis, // Prevents text overflow
+        ),
+        onTap: () {
+          print("abcd");
+
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => CurrentAddressMapScreen(latitude: this.latitude, longitude: this.longitude,))
+          );
+        },
       ),
       centerTitle: true,        // Center the title properly
       titleSpacing: 0,          // Adjust title spacing to avoid overlap
